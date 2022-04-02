5020f36fc0ed2576afac37688c28a45827fc9018
Nixiris <dark.killer91@hotmail.com>
2022-04-02 18:08:14 +1100

    added settings printing

--- atol-diag.py
+++ atol-diag.py
@@ -1,2 +1,28 @@
 from hashlib import new
 import wrapper.libfptr10 as libfptr10
+
+fptr = libfptr10.IFptr()
+fptr.setSingleSetting(fptr.LIBFPTR_SETTING_MODEL, str(fptr.LIBFPTR_MODEL_ATOL_AUTO))
+fptr.setSingleSetting(fptr.LIBFPTR_SETTING_PORT, str(fptr.LIBFPTR_PORT_USB));
+fptr.open()
+
+fptr.beep()
+
+settings = fptr.getSettingsStr()
+
+fptr.beginNonfiscalDocument()
+
+fptr.setParam(fptr.LIBFPTR_PARAM_TEXT, settings)
+fptr.printText()
+fptr.setParam(fptr.LIBFPTR_PARAM_CUT_TYPE,fptr.LIBFPTR_CT_PART)
+
+fptr.setParam(fptr.LIBFPTR_PARAM_BARCODE, settings)
+fptr.setParam(fptr.LIBFPTR_PARAM_BARCODE_TYPE, fptr.LIBFPTR_BT_QR)
+fptr.setParam(fptr.LIBFPTR_PARAM_SCALE, 5)
+fptr.setParam(fptr.LIBFPTR_PARAM_ALIGNMENT,fptr.LIBFPTR_ALIGNMENT_CENTER)
+fptr.printBarcode()
+
+fptr.setParam(fptr.LIBFPTR_PARAM_PRINT_FOOTER,False)
+fptr.endNonfiscalDocument()
+
+fptr.close()
